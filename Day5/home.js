5a2c562e60b005eee7c02eb08bb42566918c4ce8
Pranati Bagchi <lavender.spaceship.project@gmail.com>
2018-12-01 16:21:56 +0800

    Day5JS

---
+++ Day5/home.js
@@ -0,0 +1,40 @@
+var my_name;
+var my_age;
+var my_birthdate;
+
+my_name = "Pranati";
+my_age = 35;
+my_birthdate = "25/12/1985";
+
+console.log(my_name);
+console.log(my_age);
+console.log(my_birthdate);
+
+var my_address = "JTC launchPad";
+
+
+var my_test;
+
+console.log(typeof(my_test)); 
+console.log(typeof(my_name));
+console.log(typeof(my_age));
+
+var firstName;
+var lastName;
+
+firstName="Johnny";
+lastName="Bravo";
+space=" ";
+
+var fullName = firstName + space + lastName;
+var fullName1 = firstName + " " + lastName;
+
+console.log(fullName);
+console.log(fullName1);
+
+var qty = 2;
+var total = 200;
+var qtyTotal = qty + total;
+
+console.log(qtyTotal);
+
